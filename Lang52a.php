4a84857a849a3afb7e3c1c8ec95bc1706e6b7e9e
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-09-06 15:27:15 +0000

    update

--- Lang52a.php
+++ Lang52a.php
@@ -1,1 +1,9 @@
-<?php
+<?php 
+    $length = (Int)readLine('Enter length: ');
+    $width = (Int)readLine('Enter width: ');
+
+    $area = $length * $width;
+    $perim = 2 * $length + 2 * $width;
+    echo "The area is: " . $area;
+    echo "The perimeter is: " . $perim
+?>
