83ecb86e61de1f7151e2f1ef05bde18a7daf5b01
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-09-12 15:29:31 +0000

    update 9/12

--- Lang72t.c
+++ Lang72t.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 int main() {
     int time1;
     int time2;
+    int diff;
 
     printf("Enter first time: ");
     scanf("%d", &time1);
     printf("Enter second time: ");
     scanf("%d", &time2);
 
-    
+    if (time1 > time2) {
+        diff = abs(time1 - 2460) + time2;
+    } 
+    else if (time2 > time1) {
+        diff = time2 - time1;
+    }
+    printf("%d", diff);
+    printf("\n");
 }
