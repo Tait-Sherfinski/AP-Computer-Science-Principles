c0736a9576077226c1e18d0c1fff6b159939979c
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-10-04 14:12:32 +0000

    update

--- Prog122d.c
+++ Prog122d.c
@@ -4,7 +4,7 @@
 int main() {
     for (int i = -13; i < 16; i++) {
         int x = i + 1;
-        float y = pow(x, 6) - (3 * pow(x, 5)) - (93 * pow(x, 4)) + (87 * pow(x, 3)) + (1596 * pow(x, 2)) - (1380 * x) - 2800;
+        float y = 1.11; // pow(x, 6) - (3 * pow(x, 5)) - (93 * pow(x, 4)) + (87 * pow(x, 3)) + (1596 * pow(x, 2)) - (1380 * x) - 2800;
         printf("%.2f", y); 
     }
 }
