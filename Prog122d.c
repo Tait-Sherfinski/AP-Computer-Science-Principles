e7407647ff5d9084dc049f5dc3a1c632b0ae35bf
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-10-04 14:37:45 +0000

    update 10/4

--- Prog122d.c
+++ Prog122d.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
-#include <math.h>
+
+int power(int base, int exponent) {
+    if (exponent == 0)
+        return 1;
+    else if (exponent < 0)
+        return 1 / power(base, -exponent);
+    else if (exponent % 2 == 0) {
+        float half_pow = power(base, exponent / 2);
+        return half_pow * half_pow;
+    }
+    else
+        return base * power(base, exponent - 1);
+}
 
 int main() {
     for (int i = -13; i < 16; i++) {
         int x = i + 1;
-        float y = 1.11; // pow(x, 6) - (3 * pow(x, 5)) - (93 * pow(x, 4)) + (87 * pow(x, 3)) + (1596 * pow(x, 2)) - (1380 * x) - 2800;
-        printf("%.2f", y); 
+        int y = power(x, 6) - (3 * power(x, 5)) - (93 * power(x, 4)) + (87 * power(x, 3)) + (1596 * power(x, 2)) - (1380 * x) - 2800;
+        printf("%d", y); 
+        printf("\n");
     }
 }
---
+++ Prog122h.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int sq;
+    int sqroot;
+    int cube;
+    int fthroot;
+    
+    for (int i = 1; i <= 20; i++) {
+        sq = i * i;
+        cube = i * i * i;
+    }
+}
