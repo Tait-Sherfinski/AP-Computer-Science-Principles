d87ed3cb7ec3aad5eed8a04b78f79450981b14eb
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-10-08 15:29:47 +0000

    update 10/8

--- Prog122h.c
+++ Prog122h.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <math.h>
+
+int power(int base, int exponent) {
+    return base * power(base, exponent - 1);
+}
 
 int main() {
     int sq;
-    int sqroot;
+    float sqroot;
     int cube;
-    int fthroot;
+    float fthroot;
+
+    printf("Number | Square | Square Root | Cube | 4th Root");
     
     for (int i = 1; i <= 20; i++) {
         sq = i * i;
+        sqroot = power(i, 1);
         cube = i * i * i;
+        //fthroot = squareroot(sqroot);
+        printf("\n%d " , i);
+        printf("\t%d ", sq);
+        printf("\t%.2f ", sqroot);
     }
 }
