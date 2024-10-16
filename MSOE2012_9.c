490205e005b013655304c338631f0c0581d8e576
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-10-16 15:46:17 +0000

    update 10/16

--- MSOE2012_9.c
+++ MSOE2012_9.c
@@ -3,6 +3,17 @@
 
 int main() {
     char rn;
+    int total = 0;
     printf("Enter a roman numeral: ");
-    scanf("%c", &rn);
+    scanf("%s", rn);
+    switch (rn){
+        case 'I': total += 1;
+        case 'V': total += 5;
+        case 'X': total += 10;
+        case 'L': total += 50;
+        case 'C': total += 100;
+        case 'D': total += 500;
+        case 'M': total += 1000;
+    }
+    printf("%d", total);
 }
