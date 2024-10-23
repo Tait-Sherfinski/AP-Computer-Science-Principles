41239b98bfd2168597deeb161d42d6b092f1f6eb
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-10-23 03:05:28 +0000

    update

--- MSOE2008_6.c
+++ MSOE2008_6.c
@@ -4,13 +4,14 @@
 int main () {
     char sequence;
     char target;
-    int count;
+
     printf("Enter a DNA sequence: ");
     scanf("%s", sequence);
-    printf("Enter target sequence: ");
+    printf("Enter a target sequence: ");
     scanf("%s", target);
-    char thing = strstr(sequence, target);
 
-    if (thing == NULL) printf("The target sequence appears 0 times");
-    else printf("Something");
+    int count = 0;
+    char inSequence = strstr(sequence, target);
+
+    printf("%s", inSequence);
 }
