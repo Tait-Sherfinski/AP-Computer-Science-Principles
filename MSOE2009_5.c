1a8d7d1b82919b0e0ab5f7d673ef771a5cc24ed8
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-11-01 18:31:31 +0000

    update

---
+++ MSOE2009_5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0) {
+        return true;
+    } return false;
+}
+
+int main() {
+    int num;
+    printf("Enter a non-negative number: ");
+    scanf("%d", &num);
+    bool result = isPrime(num);
+    if (result == true) printf("The number is prime");
+    else printf("The number is not prime");
+}
