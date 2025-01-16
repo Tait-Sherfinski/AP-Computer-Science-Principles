8bf3bd0fc1666cf3d747eeaf09faebfd5e09c313
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-01-16 16:22:00 +0000

    update 1/16

--- Cl408b.py
+++ Cl408b.py
@@ -1,4 +1,22 @@
 class SortingAlgorithms:
     
-    def binarySearch():
+    def linear_search(list, target):
+        for index in range(len(list)):
+            if list[index] == target:
+                return index
+        return -1
+
+    def binary_search(list, target):
+        low = 0
+        high = len(list) - 1
+        mid = 0
+        while low <= high:
+            mid = (high + low) // 2
+            if list[mid] < target:
+                low = mid + 1
+            elif list[mid] > x:
+                high = mid - 1
+            else:
+                return mid
+        return -1
         
--- Prog408b.py
+++ Prog408b.py
@@ -1,12 +1,14 @@
-# Import time, searching library
+import time
+from Cl408b import SortingAlgorithms
 
 def main():
     try:
         data = []
-        with open("data/prog408b.dat", 'r') as f:
+        with open("data/Prog408b.dat", 'r') as f:
             data = [int(line) for line in f]
         target = int(input("Enter the number to search: "))
-        ...
+        result = binary_search(data, target)
+        print("Element found at index ", str(result))
     except Exception as e:
         print("Error:", e)
     pass
