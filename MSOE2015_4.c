991dd426dc6932d6476710f9aeb5f95d5a3ae1e2
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-04-04 15:25:38 +0000

    update 4/4

--- MSOE2015_4.c
+++ MSOE2015_4.c
@@ -2,8 +2,12 @@
 
 int main() {
     float a,b,c,d,e,f,g,h,i,j;
-    float[] list;
+    float list[10];
     printf("Enter 10 numbers with decimals\n");
     printf("1: ");
     scanf("%f", &a);
+    printf("2: ");
+    scanf("%f", &b);
+    printf("3: ");
+    scanf("%f", &c);
 }
