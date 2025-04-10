9c72bdb5f29e3a70752b5819249167301e596ca7
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-04-10 15:27:49 +0000

    update 4/10

--- MSOE2010_6.c
+++ MSOE2010_6.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-float func(int n, int x[], int y[]) {
+float func(int n, float x[], float y[]) {
     float A = 0.0;
     for (int i = 0; i < n-1; i++) {
         A += x[i]*y[i+1] - y[i]*x[i+1];
-    } return A;
+    } printf("%f", A);
 }
 
 int main() {
     int n;
-    int x[n];
-    int y[n];
+    float temp;
+    float temp2;
+    float x[n];
+    float y[n];
     printf("Enter number of vertices: ");
     scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("X coordinate of vertex %d: ", i+1);
+        scanf("%f", &temp);
+        x[i] = temp;
+        printf("Y coordinate of vertex %d: ", i+1);
+        scanf("%f", &temp2);
+        y[i] = temp2;
+    }
     func(n, x, y);
 }
