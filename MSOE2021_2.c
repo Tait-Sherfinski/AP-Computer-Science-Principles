ca144977dc5c4669d3f8c03c9c3d70dfd054ae7a
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-04-17 15:27:14 +0000

    update 4/17

--- MSOE2021_2.c
+++ MSOE2021_2.c
@@ -2,19 +2,20 @@
 
 int main() {
     int water;
-    int cups[5];
+    int cups[100];
 
     printf("Enter OZs of water remaining: ");
     scanf("%d", &water);
     printf("Cups of coffee(4, 6, or 8 oz): ");
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 100; i++) {
         scanf("%d", &cups[i]);
+        break;
     }
-    for (int i = 0; i < 5; i++) {
-        water -= cups[i] + 1;
-        if (water < (cups[i] +1)) {
-            printf("%d cups filled", i + 1);
-            return 0;
+    for (int i = 0; i < 100; i++) {
+        water -= (cups[i] + 1);
+        if (water < 10) {
+            break;
+            printf("%d", i);
         }
-    }
+    } 
 }
